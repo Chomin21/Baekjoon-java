86b4ce1c889c55236a047c4d165dc5e0c0f0ad0e
Chomin21 <cmkbooty21@naver.com>
2022-12-02 23:28:53 +0900

    1076번-저항

---
+++ Baekjoon-java/src/bronze/Resistance.java
@@ -0,0 +1,33 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Resistance {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String[] arr = { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+		
+		String temp = "";
+		Long answer = 0L;
+		for (int i = 0; i < 3; i++) {
+			String str = br.readLine();
+			for(int j = 0; j<10;j++) {
+				if(arr[j].equals(str)) {
+					if(i == 2) {
+						answer = (long) (Long.parseLong(temp) * (Math.pow(10, j)));
+					}else {
+						temp += String.valueOf(j);
+					}
+					break;
+				}
+			}
+		}
+		System.out.println(answer);
+
+	}
+
+}
