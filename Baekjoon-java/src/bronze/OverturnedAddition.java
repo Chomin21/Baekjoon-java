f237a507f70e00747e66d85a27782f31545002d8
Chomin21 <cmkbooty21@naver.com>
2022-12-04 17:50:03 +0900

    1357번-뒤집힌 덧셈

---
+++ Baekjoon-java/src/bronze/OverturnedAddition.java
@@ -0,0 +1,31 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class OverturnedAddition {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] inputs = br.readLine().split(" ");
+		int answer = 0;
+		
+		for(String s : inputs) {
+			answer += rev(s);
+		}
+		
+		answer = rev(String.valueOf(answer));
+		System.out.println(answer);
+	}
+	
+	public static int rev(String s) {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(s);
+		sb.reverse();
+		
+		return Integer.parseInt(sb.toString());
+	}
+
+}
