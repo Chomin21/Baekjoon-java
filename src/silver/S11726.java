b248f534e004a646c364b55adea3174092cd1e41
chominkyoung <mk.jo@amorepacific.com>
2025-04-30 16:51:00 +0900

    S11726 - 2xn 타일링

--- src/silver/S11726.java
+++ src/silver/S11726.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
 
+//2xn 타일링
 public class S11726 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
